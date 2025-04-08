cd30f75623fb8198ad681298b7f8dea5025a34be
Alexandr Protsenko <itsprotsenko@gmail.com>
2025-04-08 14:49:42 -0400

    Zigzag Conversion

--- src/main/java/com/alex/Main.java
+++ src/main/java/com/alex/Main.java
@@ -2,7 +2,7 @@ package com.alex;
 
 public class Main {
     public static void main(String[] args) {
-        MedianOfTwoSortedArrays m = new MedianOfTwoSortedArrays();
-        System.out.println(m.findMedianSortedArrays(new int[] { 1, 2, 3 }, new int[] { 4, 6 }));
+        ZigzagConversion a = new ZigzagConversion();
+        System.out.println(a.convert("A", 3));
     }
 }
