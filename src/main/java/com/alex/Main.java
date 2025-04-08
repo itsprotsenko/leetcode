8c618414fb18544646ee81bcee6657fee51907cc
Alexandr Protsenko <itsprotsenko@gmail.com>
2025-04-08 13:26:47 -0400

    Median of Two Sorted Arrays

--- src/main/java/com/alex/Main.java
+++ src/main/java/com/alex/Main.java
@@ -2,7 +2,7 @@ package com.alex;
 
 public class Main {
     public static void main(String[] args) {
-        LongestPalindromicSubstring sub = new LongestPalindromicSubstring();
-        System.out.println(sub.longestPalindrome("abc"));
+        MedianOfTwoSortedArrays m = new MedianOfTwoSortedArrays();
+        System.out.println(m.findMedianSortedArrays(new int[] { 1, 2, 3 }, new int[] { 4, 6 }));
     }
 }
--- src/main/java/com/alex/MedianOfTwoSortedArrays.java
+++ src/main/java/com/alex/MedianOfTwoSortedArrays.java
@@ -1,4 +1,26 @@
 package com.alex;
 
 public class MedianOfTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums = new int[nums1.length + nums2.length];
+
+        int x=0, y=0;
+        for(int i = 0; i < nums.length; i++) {
+            if(x<nums1.length && (y==nums2.length || nums1[x]<=nums2[y])) {
+                nums[i] = nums1[x];
+                x++;
+            }
+            else {
+                nums[i] = nums2[y];
+                y++;
+            }
+        }
+
+        if(nums.length % 2 == 0) {
+            return (double) (nums[nums.length / 2] + nums[(nums.length / 2) - 1]) /2;
+        }
+        else {
+            return nums[nums.length / 2];
+        }
+    }
 }
